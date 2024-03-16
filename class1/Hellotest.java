00bfb15261f64c905332b15b238a32ca3a5f2c29
Subrata Shawon <96424696+Subrata1608024@users.noreply.github.com>
2024-03-16 21:49:14 +0600

    first commit

---
+++ class1/Hellotest.java
@@ -0,0 +1,9 @@
+package class1;
+
+public class Hellotest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("hello world2");
+        System.out.println("Hello world");
+    }
+}
---
+++ class1/More_class.java
@@ -0,0 +1,8 @@
+package class1;
+
+public class More_class {
+    public static void main(String[] args) {
+     int number=60;
+        System.out.println(number++);
+    }
+}
